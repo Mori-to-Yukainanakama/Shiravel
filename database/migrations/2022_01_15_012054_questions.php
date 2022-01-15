dd9651432e84aa7b2264bdd5973b7397dfb4cde1
morikouki <akzouksia@icloud.com>
2022-01-15 10:46:48 +0900

    questions_migratino 作成

---
+++ database/migrations/2022_01_15_012054_questions.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Questions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('question_id');
+            $table->integer('user_id')->nullable(false);
+            $table->string('title', 255)->nullable(false);
+            $table->string('content', 255)->nullable(false);
+            $table->boolean('is_answered')->default(false);
+            $table->boolean('is_solved')->default(false);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
