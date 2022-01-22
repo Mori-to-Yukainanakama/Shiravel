a2ca9b4d0d331651993e6459386980d669bae697
kumikoasano <kumiko.asano@fox_hound.tech>
2022-01-22 11:44:04 +0900

    回答api作成

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'answer'], function () {
+    // 回答api
+    Route::group(['prefix' => 'answer'], function () {
+        Route::get('/', [AnswerController::class, 'answerPost']);
+    });
+});
