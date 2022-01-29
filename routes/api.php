bfd19f25bfb9ed54fc6b427fd1c9c237a987f637
shinya.fujii <shinya.fujii@fox-hound.tech>
2022-01-29 09:36:12 +0900

    テストコード作成

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuestionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'v1'], function () {
+    // 質問
+    Route::group(['prefix' => 'questions'], function () {
+        Route::get('/', [QuestionController::class, 'index']);
+    });
 });
