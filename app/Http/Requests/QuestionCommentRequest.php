a065effd2a7d152ce71d243d8e1addb3b9336a14
SotaTsurushima <2018cassotatsurushima@gmail.com>
2022-03-20 10:54:42 +0900

    質問コメントAPI validationの作成

--- app/Http/Requests/QuestionCommentRequest.php
+++ app/Http/Requests/QuestionCommentRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
 
 class QuestionCommentRequest extends FormRequest
 {
@@ -13,7 +16,15 @@ class QuestionCommentRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        // trueにしたらフォームリクエストの利用が許可される
+        return true;
+
+        // 以下のようにすれば、パスごとに適用できる
+        // if ($this->path() == 'sample') {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
     }
 
     /**
@@ -24,8 +35,38 @@ class QuestionCommentRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            // 数字かどうか
+            'user_id' => 'required|numeric',
+            'question_id' => 'required|numeric',
+            'content' => 'required|max:255',
         ];
+    }
+
+    public function messages()
+    {
+        return [
+            // 回答内容がない時のエラーメッセージ
+            'content.required' => '回答内容は必ず入力してください',
+
+            'content.max' => '回答内容が長すぎます。255文字以内で投稿してください',
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator
+     * @return void
+     * @throw HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $data = [
+            'message' => __('バリデーションエラーが発生しました。'),
+            'errors' => $validator->errors()->toArray(),
+        ];
+
+        throw new HttpResponseException(response()->json($data, 422));
     }
 }
 
